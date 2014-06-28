b2a49ad2fc5b562cc766f736609b9beead809177
company <company@pm.com>
2014-06-28 13:45:13 +0800

    Signed-off-by: company <company@pm.com>

--- pm/order_edit.php
+++ pm/order_edit.php
@@ -1,19 +1,30 @@
 <?php
 require_once(dirname(__FILE__)."/../include/common.inc.php");
 
-$uptime = date("Y-m-d H:i:s",time());
-
-//echo $template;
-
-$inquery = "INSERT INTO `#@__order`(`name`,`phone`,`template`,`domain`,`price`,`message`,`uptime`)
-                   VALUES ('$name','$phone','$template','$domain','$price','$message','$uptime'); ";
-$rs = $dsql->ExecuteNoneQuery($inquery);
-if(!$rs)
-{
-	ShowMsg(' 修改错误! ', '-1');
-	echo $dsql->GetError();
-	exit();
-}else{
-	echo $uptime;
+if($dopost == "change"){
+	$uptime = date("Y-m-d H:i:s",time());
+	$id = intval($id);			
+	$inquery = "UPDATE `#@__order` SET name='$name', phone='$phone', template='$template', price='$price', message='$message', uptime='$uptime', domain='$domain' WHERE id='$id'";
+					   
+	//$dsql->ExecuteNoneQuery("UPDATE `#@__channeltype` SET fieldset='$oksetting' WHERE id='$id' ");
+	$rs = $dsql->ExecuteNoneQuery($inquery);
+	if(!$rs)
+	{
+		echo "修改失败";
+		exit();
+	}else{
+		echo $uptime;
+	}
+}elseif($dopost == "drop"){
+	$rs = $dsql->ExecuteNoneQuery("DELETE FROM `#@__order` WHERE id = '$uid' ");
+	if(!$rs)
+	{
+		echo "删除失败";
+		exit();
+	}else{
+		echo "删除成功";
+	}
 }
+
+
 ?>
